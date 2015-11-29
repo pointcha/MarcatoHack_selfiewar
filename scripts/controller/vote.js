82ea800c7c7670db280fd69d9533e6a8c40f1301
drrnmcdnld <drrnmcdnld@gmail.com>
2015-11-29 12:53:59 -0400

    Added voting functionality but it currently isn't working.

--- scripts/controller/vote.js
+++ scripts/controller/vote.js
@@ -1,4 +1,4 @@
-app.controller('MainCtrl', function($scope) {
+app.controller('AuthController', function($scope) {
   $scope.changeVote = function(vote, flag) {
     $scope.vote = vote == flag ? 'None' : flag;
     alert($scope.vote);
