f48bf4d30c2d200541a3d4fe7af6d2e3d92b97d9
pointcha <info@pointcha.ca>
2015-11-29 15:10:07 -0400

    pull pull pull

--- scripts/snap.js
+++ scripts/snap.js
@@ -2,6 +2,8 @@
 
 var myDataRef = new Firebase('https://selfiewar.firebaseio.com/'); //point to firebase
 var uName = "";
+var fbItems = [];
+var voteItems = [];
 //myDataRef.child('challenge').child("Marcato Hackathon").set({ title: 'Marcato Hackathon', prize: 'A slice of pizza!'});
 
 function uploadPhoto() {
@@ -11,15 +13,12 @@ var authData = myDataRef.getAuth();
     var userId = authData.uid;
 
 
-myDataRef.child('user').child(userId).once("value", function(snapshot) {
-    var data = snapshot.val();
-    if (name) {
-        uName = data.name;
-    }
-}); 
-
-    console.log('user is already logged in');
-  } 
+    myDataRef.child('user').child(userId).once("value", function(snapshot) {
+            var data = snapshot.val();
+            if (name) { uName = data.name; }
+        }); 
+        console.log('user is already logged in');
+    } 
 
     var srcData;
     var score = 0;
@@ -32,9 +31,6 @@ myDataRef.child('user').child(userId).once("value", function(snapshot) {
         
         fileReader.onload = function(fileLoadedEvent) {
             srcData = fileLoadedEvent.target.result; // <--- data: base64
-
-            console.log(score + " " + uName + " " + userId);
-
             var newImage = document.createElement('img');
             newImage.src = srcData;
 
@@ -48,6 +44,35 @@ myDataRef.child('user').child(userId).once("value", function(snapshot) {
 }
 
 function pullPhotos() {
-    
+    // pull array or pics from challenge|Marcato Hacathon|pics|userId
+    myDataRef.child('challenge').child('Marcato Hackathon').child('pics').once("value", function(snapshot) {
+
+snapshot.forEach(function(childSnapshot) {
+    var key = childSnapshot.val();
+
+fbItems.push(key);
+  });
+
+var arraynumber = fbItems.length; // how many items in the array
+    var voteItems = shuffleArray(fbItems, arraynumber);
+
+});  
+}
+
+function shuffleArray(array, numberinarray) {
+// shuffle voteItem array
+for (var i = numberinarray - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+console.log(array);
+    // pull last 2 items into final array
+    var challengearray = array.slice(Math.max(array.length - 2, 1))
+    console.log(challengearray);
+    return challengearray;
+
+    // shuffle voteItem array
 }
 
