ec98da859cd45d28732af161ad76d2ae15b86dbb
pointcha <info@pointcha.ca>
2015-11-29 12:47:11 -0400

    Post system working--MIC DROP

--- scripts/snap.js
+++ scripts/snap.js
@@ -1,9 +1,26 @@
 'use strict';
 
 var myDataRef = new Firebase('https://selfiewar.firebaseio.com/'); //point to firebase
+var uName = "";
 //myDataRef.child('challenge').child("Marcato Hackathon").set({ title: 'Marcato Hackathon', prize: 'A slice of pizza!'});
 
 function uploadPhoto() {
+
+var authData = myDataRef.getAuth();
+    if (authData){
+    var userId = authData.uid;
+
+
+myDataRef.child('user').child(userId).once("value", function(snapshot) {
+    var data = snapshot.val();
+    if (name) {
+        uName = data.name;
+    }
+}); 
+
+    console.log('user is already logged in');
+  } 
+
     var srcData;
     var score = 0;
     var file = document.getElementById('photoUpload').files[0];
@@ -16,12 +33,14 @@ function uploadPhoto() {
         fileReader.onload = function(fileLoadedEvent) {
             srcData = fileLoadedEvent.target.result; // <--- data: base64
 
+            console.log(score + " " + uName + " " + userId);
+
             var newImage = document.createElement('img');
             newImage.src = srcData;
 
             if(file){
 
-                myDataRef.child('challenge').child('Marcato Hackathon').child('pics').set({ photo: srcData, score: score,  name: name});
+                myDataRef.child('challenge').child('Marcato Hackathon').child('pics').child(userId).set({ photo: srcData, score: score, name: uName});
             }
         }
         fileReader.readAsDataURL(fileToLoad);
